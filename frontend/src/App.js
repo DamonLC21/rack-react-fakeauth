53d649129295cb53b04594b8270645e30cc26136
DamonLC21 <chiversdamon@gmail.com>
2021-05-11 09:52:59 -0600

    solution

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,38 @@ import {Component} from 'react'
 
 class App extends Component {
   state = {
+    user: {}
+  }
+
+  login = (username) => {
+    return fetch('http://localhost:9393/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      if(response.error){
+        alert(response.error)
+      }
+      else{
+        this.setState({
+          user: response
+        })
+      }
+      return response
+    })
 
   }
 
   render(){
     return (
       <div className="App">
-        <Route path="/" exact render={(props) => <Login {...props} /> } />
+        <Route path="/" exact render={(props) => <Login {...props} login={this.login} /> } />
         <Route path="/home" exact render={(props) => <Home {...props} /> } />
       </div>
     );
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react'
 
 export default class Login extends Component {
+
+  state = {
+    username: "",
+    error: ""
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    this.props.login(this.state.username)
+      .then(res => {
+        if(res.id){
+          this.props.history.push('/home')
+        }
+        else {
+          this.setState({
+            error: res.error
+          })
+        }
+      })
+  }
+
   render() {
     return (
       <div>
         <p>Please Log In</p>
+        <form onSubmit={this.handleSubmit}>
+          <input 
+            type="text" 
+            name="username" 
+            value={this.state.username} 
+            onChange={this.handleChange}
+          />
+          <input type="submit" />
+        </form>
+        {this.state.error ? <p>{this.state.error}</p> : null }
       </div>
     )
   }
